9e84c5cac4901fe23e46093a28b52ab949259af8
Felix Ondullah <felixondullah@gmail.com>
2022-10-03 08:23:53 +0300

    completed

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -3,9 +3,17 @@ import { Route } from "react-router-dom";
 import MoviesList from "./MoviesList";
 
 function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+  
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+      <Route exact path={`${match.url}/:movieId`}>
+        <MoviesShow movies={movies}/>
+      </Route>
     </div>
   );
 }
